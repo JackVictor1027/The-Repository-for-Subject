af1701d66b2f6cb8dd0d5fa6088221b7fb5a0f45
Jack Victor <122539141+JackVictor1027@users.noreply.github.com>
2023-08-11 09:56:38 +0800

    Create user.go

---
+++ Like/handler/user.go
@@ -0,0 +1,37 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func LikeThisVideo(c *gin.Context) {
+
+}
+func GetLikelist(c *gin.Context) {
+
+}
+
+func GetLikes(c *gin.Context) {
+
+}
+func CancelLike(c *gin.Context) {
+
+}
+
+// GetUserInfo 获取用户信息
+func GetUserInfo(c *gin.Context) {
+
+}
+
+// UpdateUserInfo 更新用户信息
+func UpdateUserInfo(c *gin.Context) {
+
+}
+
+// DeleteUser 删除用户
+func DeleteUser(c *gin.Context) {
+
+}
+
+// 添加用户
+func AddUser(c *gin.Context) {
+
+}
